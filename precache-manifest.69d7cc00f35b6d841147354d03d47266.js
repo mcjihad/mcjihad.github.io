a782e3dd97a06fa95c89a0668bdf19d251831d36
mcjihad <mcjihad99@gmail.com>
2020-07-02 00:25:09 +0700

    menyalin isi folder build

---
+++ precache-manifest.69d7cc00f35b6d841147354d03d47266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c04ecc959528280a4b41",
+    "url": "/static/css/main.e1080ffb.chunk.css"
+  },
+  {
+    "revision": "c04ecc959528280a4b41",
+    "url": "/static/js/main.c04ecc95.chunk.js"
+  },
+  {
+    "revision": "3ec5c86788adc9a1050f",
+    "url": "/static/js/1.3ec5c867.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "93cbf18838c4afe54e9135929cbfeb8e",
+    "url": "/index.html"
+  }
+];
